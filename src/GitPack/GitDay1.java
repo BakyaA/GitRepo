4f4529ad5020333fbff71bb35dcbb80b77131107
BakyaA <bakya.amirtharaj@gmail.com>
2021-02-18 00:35:18 +0530

    jhansi socond updated code

--- src/GitPack/GitDay1.java
+++ src/GitPack/GitDay1.java
@@ -4,6 +4,6 @@ public class GitDay1 {
  public static void main(String[] args) {
 	 System.out.println("jhansi started");
 	 System.out.println("Anu task started");
-
+      System.out.println("Task done with pulling from second user");
 }
 }
